2028f6a0770948e76b35c868c5b169766b86d410
Brambs <gabriel128.br@gmail.com>
2023-05-05 14:02:09 -0300

    Card var

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,9 +5,10 @@ import { Stepper } from '../Stepper'
 import favIcon from '../../assets/icons/Heart.svg'
 // eslint-disable-next-line react/prop-types
 export function Card({ img, price, title, ...rest }) {
+  let icon = favIcon
   return (
     <Container {...rest}>
-      <img className="Icon" src={favIcon} alt="Top left icon" />
+      <img className="Icon" src={icon} alt="Top left icon" />
       <img src={img} alt="Dishe ilustrative image">
 
       </img>

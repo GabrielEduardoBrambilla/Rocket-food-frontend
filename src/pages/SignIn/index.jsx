62e8ba2eedaf6a5169375e0cf0d5befc35af1926
Brambs <gabriel128.br@gmail.com>
2023-05-13 23:32:11 -0300

    Update index.jsx

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,10 @@
 import { Header } from '../../components/Header'
-import dishImg from '../../assets/dishes/Dish.png'
-import { Card } from '../../components/Card'
 import { Container } from "./styles"
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <Card img={dishImg} title="Passion fruit Juice" price="10,25" />
     </Container>
   )
 }

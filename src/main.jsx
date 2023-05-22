08693cebe6804f07768468909dee7f63cadecd4e
Brambs <gabriel128.br@gmail.com>
2023-05-22 11:09:14 -0300

    User register

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-
-
+import { Routes } from './routes'
 
 import theme from './styles/theme'
-import { Home } from './pages/Home'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,22 +2,49 @@ import { Container } from "./styles"
 import { Logo } from '../../components/Logo'
 import { Input } from '../../components/Input'
 import { IncludeButton } from '../../components/IncludeButton'
+
+import { useState } from "react"
+import { api } from "../../services/api";
+import { useNavigate } from 'react-router-dom';
+
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+        navigate("/");
+      }).catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar.");
+        }
+      });
+  }
+
   return (
     <Container>
       <Logo formHeader />
       <form action="">
         <p className="desktop-title">Create your account</p>
         <label htmlFor="user-name">Your name
-          <Input id='user-name' label='email' type='text' placeholder='John Don' />
+          <Input id='user-name' label='email' type='text' placeholder='John Don' onChange={e => setName(e.target.value)} />
         </label>
         <label htmlFor="email">Email
-          <Input id='email' label='email' type='text' placeholder='example@gmail.com' />
+          <Input id='email' label='email' type='text' placeholder='example@gmail.com' onChange={e => setEmail(e.target.value)} />
         </label>
         <label htmlFor="password">Password
-          <Input id='password' placeholder='password' />
+          <Input id='password' placeholder='password' onChange={e => setPassword(e.target.value)} />
         </label>
-        <IncludeButton title='Create account' type='submit' />
+        <IncludeButton title='Create account' onClick={handleSignUp} />
         <a href="#">Login here!</a>
       </form>
     </Container>

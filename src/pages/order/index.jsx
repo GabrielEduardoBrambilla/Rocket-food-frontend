1189c5a354280b55695af9bab8eabb93eb525dc6
Brambs <gabriel128.br@gmail.com>
2023-07-02 18:10:22 -0300

    User order

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -1,29 +1,46 @@
 import { Container } from "./styles";
 
 import { Header } from "../../components/Header";
-import { useEffect } from "react";
+import { useState, useEffect } from "react"
 import { useAuth } from '../../hooks/auth'
+import { api } from "../../services/api";
+import { Stepper } from "../../components/Stepper";
 
 export function Order() {
   const { user } = useAuth()
+  const [quantity, setQuantity] = useState(1);
+  const [userOrder, setUserOrder] = useState([]);
+
+
   useEffect(() => {
 
     async function fetchApi() {
-
+      user.id
+      const { data } = await api.get(`/order/index/${user.id}`);
+      setQuantity(quantity)
+      setUserOrder(data)
+      console.log(userOrder)
     }
 
     fetchApi()
-  }, [])
-
+  }, [quantity, user.id])
   return (
     <Container>
       <Header />
       <div className="order-info-wrapper">
         <p>My order</p>
         {
-          // show every item in the order
+          userOrder.map((item) => {
+            return (
+              <div key={String(item.dish_id)}>
+                <img src="" alt={item.dish_img} />
+                <p>{item.dish_name}</p>
+                <Stepper quantity={item.quantity} setQuantity={setQuantity} />
+                <span>Total</span>
+              </div>
+            )
+          })
         }
-        <span>Total: ${ }</span>
       </div>
       <div className="payment-wrapper">
 

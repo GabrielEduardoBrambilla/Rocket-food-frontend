bdc62a59203b6855572a058c2ce68eb83936d3ab
Brambs <gabriel128.br@gmail.com>
2023-07-03 11:32:39 -0300

    Order List css

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -5,42 +5,91 @@ import { useState, useEffect } from "react"
 import { useAuth } from '../../hooks/auth'
 import { api } from "../../services/api";
 import { Stepper } from "../../components/Stepper";
+import loading from "../../assets/icons/loading.jpg";
+import { FaTrashAlt } from "react-icons/fa"
 
 export function Order() {
   const { user } = useAuth()
   const [quantity, setQuantity] = useState(1);
   const [userOrder, setUserOrder] = useState([]);
-
+  const [images, setImages] = useState({});
+  const orderTotalPrice = userOrder.reduce(
+    (accumulator, item) =>
+      accumulator + item.quantity * item.item_price_at_time,
+    0
+  )
 
   useEffect(() => {
 
     async function fetchApi() {
-      user.id
       const { data } = await api.get(`/order/index/${user.id}`);
       setQuantity(quantity)
+      console.log(quantity)
       setUserOrder(data)
-      console.log(userOrder)
+
+      const imgNames = data.map((item) => item.dish_img);
+      await Promise.all(imgNames.map(fetchImage)); // Fetch all images concurrently
+
+      function fetchImage(imageName) {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = `${api.defaults.baseURL}/files/${imageName}`;
+          img.onload = () => {
+            setImages((prevImages) => ({
+              ...prevImages,
+              [imageName]: img.src,
+            }));
+            resolve();
+          };
+          img.onerror = () => {
+            setImages((prevImages) => ({
+              ...prevImages,
+              [imageName]: loading,
+            }));
+            reject();
+          };
+        });
+      }
     }
 
     fetchApi()
-  }, [quantity, user.id])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user.id])
   return (
     <Container>
       <Header />
       <div className="order-info-wrapper">
-        <p>My order</p>
+        <h2>My Order</h2>
         {
           userOrder.map((item) => {
             return (
-              <div key={String(item.dish_id)}>
-                <img src="" alt={item.dish_img} />
-                <p>{item.dish_name}</p>
-                <Stepper quantity={item.quantity} setQuantity={setQuantity} />
-                <span>Total</span>
+              <div className="order_item" key={String(item.dish_id)}>
+                <img className="dish_img left-section" src={images[item.dish_img] || loading} alt={item.dish_img} />
+                <div className="middle-section">
+                  <p>{item.dish_name}</p>
+                  <Stepper
+                    className="stepper"
+                    quantity={item.quantity}
+                    setQuantity={(newQuantity) => {
+                      setUserOrder((prevOrder) =>
+                        prevOrder.map((orderItem) =>
+                          orderItem.dish_id === item.dish_id
+                            ? { ...orderItem, quantity: newQuantity }
+                            : orderItem
+                        )
+                      );
+                    }}
+                  />
+                </div>
+                <FaTrashAlt className="right-section " />
               </div>
+
             )
           })
         }
+        <span>
+          Total: {orderTotalPrice}
+        </span>
       </div>
       <div className="payment-wrapper">
 
--- src/pages/order/styles.js
+++ src/pages/order/styles.js
@@ -1,10 +1,77 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex; */
   flex-direction: column;
   height: 100vh;
   background-color: ${({ theme }) => theme.COLORS.DARK[400]};
+
+  .order-info-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 350px;
+    margin: auto;
+    gap: 25px;
+
+    > span {
+      font-size: 20px;
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 160%;
+    }
+
+    > h2 {
+      color: ${({ theme }) => theme.COLORS.LIGHT[300]};
+      font-size: 32px;
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 140%;
+    }
+  }
+  .dish_img {
+    grid-area: 'image';
+    height: 80px;
+    width: 80px;
+    object-fit: cover;
+    border-radius: 90%;
+  }
+  .order_item {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    gap: 10px;
+    align-items: center;
+  }
+
+  .left-section {
+    grid-column: 1;
+  }
+
+  .middle-section {
+    grid-column: 2;
+    display: flex;
+    flex-direction: column;
+
+    > p {
+      font-size: 20px;
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 160%;
+    }
+  }
+
+  .right-section {
+    grid-column: 3;
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
+  }
+
+  .dele_icon {
+    align-self: center;
+  }
 
   @media (min-width: 768px) {
     background-color: ${({ theme }) => theme.COLORS.DARK[400]};
